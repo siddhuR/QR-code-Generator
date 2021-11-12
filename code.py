18c7d799b7c8dc79edeb721181e30d68d38e135b
siddhuR <92938829+siddhuR@users.noreply.github.com>
2021-11-12 11:37:53 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,77 @@
+from tkinter import*
+import qrcode
+from tkinter.font import BOLD
+class Qr_Generator:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("900x500+200+50")
+        self.root.title("QR_Generator | Developed by Siddhartha")
+        self.root.resizable(False,False)
+
+        title=Label(self.root,text="Qr code Generator",font=("times new roman",40),bg='#053246',fg='white').place(x=0,y=0,relwidth=1)
+
+        #=====STUDENT DETAILS WINDOW=====
+        #====Variables====
+
+        self.var_Std_code=StringVar()
+        self.var_name=StringVar()
+        self.var_department=StringVar()
+        self.var_college=StringVar()
+
+        Std_Frame=Frame(self.root,bd=2,relief=RIDGE,bg='white')
+        Std_Frame.place(x=50,y=100,width=500,height=380)
+
+        Std_title=Label(Std_Frame,text="Student Details",font=("goudy old style",20),bg='#043256',fg='white').place(x=0,y=0,relwidth=1)
+
+        lbl_Std_code=Label(Std_Frame,text="Student Reg.No :-",font=("times new roman",15,'bold'),bg='white').place(x=20,y=60)
+        lbl_name=Label(Std_Frame,text="Name :-",font=("times new roman",15,'bold'),bg='white').place(x=20,y=100)
+        lbl_department=Label(Std_Frame,text="Department :-",font=("times new roman",15,'bold'),bg='white').place(x=20,y=140)
+        lbl_university=Label(Std_Frame,text="College/University Name :-",font=("times new roman",15,'bold'),bg='white').place(x=20,y=180)
+
+        txt_university=Entry(Std_Frame,font=("times new roman",15),textvariable=self.var_Std_code,bg='lightyellow').place(x=260,y=60)
+        txt_name=Entry(Std_Frame,font=("times new roman",15),textvariable=self.var_name,bg='lightyellow').place(x=260,y=100)
+        txt_department=Entry(Std_Frame,font=("times new roman",15),textvariable=self.var_department,bg='lightyellow').place(x=260,y=140)
+        txt_university=Entry(Std_Frame,font=("times new roman",15),textvariable=self.var_college,bg='lightyellow').place(x=260,y=180)
+
+        btn_generate=Button(Std_Frame,text='QR Generate',command=self.generate,font=("times new roman",18,'bold'),bg='#2196f3',fg='white').place(x=87,y=250,width=173,height=30)
+        btn_clear=Button(Std_Frame,text='Clear',command=self.clear,font=("times new roman",18,'bold'),bg='#607d8b',fg='white').place(x=290,y=250,width=120,height=30)
+
+        self.msg=''
+        self.lbl_msg=Label(Std_Frame,text=self.msg,font=("times new roman",20),bg='white',fg='green')
+        self.lbl_msg.place(x=0,y=310,relwidth=1)
+
+        #=====STUDENT QRcode WINDOW=====
+        qr_Frame=Frame(self.root,bd=2,relief=RIDGE,bg='white')
+        qr_Frame.place(x=600,y=100,width=250,height=380)
+
+        Std_title=Label(qr_Frame,text="Student QR code",font=("goudy old style",20),bg='#043256',fg='white').place(x=0,y=0,relwidth=1)
+
+
+        self.qr_code=Label(qr_Frame,text='No QR \nAvailable',font=('times new roman',15),bg='#3f51b5',fg='white',bd=1,relief=RIDGE)
+        self.qr_code.place(x=35,y=100,width=180,height=180)
+
+    def clear(self):
+        self.var_Std_code.set('')
+        self.var_name.set('')
+        self.var_department.set('')
+        self.var_college.set('')
+
+        self.msg=''
+        self.lbl_msg.config(text=self.msg)
+
+    def generate(self):
+        if self.var_college.get()=='' or self.var_Std_code.get()=='' or self.var_department.get()=='' or self.var_name.get()=='':
+            self.msg='All Fields are required...!'
+            self.lbl_msg.config(text=self.msg,fg='red')
+
+        else:
+            
+
+            #===UPDATING NOTIFICATION===
+            self.msg='QRcode Generated Successfully...!'
+            self.lbl_msg.config(text=self.msg,fg='green')
+
+
+root=Tk()
+obj =Qr_Generator(root)
+root.mainloop()
